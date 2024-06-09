e029adcdc7cad902830531967624b3cdb89e43c9
rudyhuacasi <alexisitaliano16@gmail.com>
2024-06-09 21:18:06 +0200

    ups: added header

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,8 +1,20 @@
-<!-- <script>
+<script>
 
-</script> -->
+</script>
 <template>
-<h1>VUE VITE TEMPLATE</h1>
-<p>il mio primo componente</p>
+    <header class="container text-center">
+        <h1 class="m-3">Rick and Morty App</h1>
+        <div class="container">
+            <input type="text" class=" m-2  d-inline w-15 form-control" placeholder="Search character" aria-label="Username">
+            <select class=" m-2  form-select d-inline w-15" id="inputGroupSelect04" aria-label="Example select with button addon">
+                <option selected>Select status</option>
+                <option value="1">Human</option>
+                <option value="2">Alien</option>
+            </select>
+      
+            <button type="button" class=" m-2 btn btn-primary">Search</button>
+            <button type="button" class=" m-2 btn btn-warning">Reset</button>
+        </div>
+    </header>
 </template>
-<!-- <style></style> -->
+<style></style>
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './assets/scss/main.scss'
+// import './assets/scss/main.scss'
 import App from './App.vue'
+
+import './assets/scss/main.scss';
+import 'bootstrap';
+
 
 createApp(App).mount('#app')
